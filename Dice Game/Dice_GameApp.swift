47f30892799859eb43663b0cd3ee7222057146ee
Jack <Jwenschhof@bsd220.org>
2022-11-17 09:59:33 -0600

    Initial Commit

---
+++ Dice Game/Dice_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dice_GameApp.swift
+//  Dice Game
+//
+//  Created by Jack W on 11/17/22.
+//
+
+import SwiftUI
+
+@main
+struct Dice_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
